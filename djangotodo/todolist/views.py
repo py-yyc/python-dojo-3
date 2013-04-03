1510a4cdd9f0fa3c49dbf070b27709d2193c35fe
Abhin Chhabra <chhabra.abhin@gmail.com>
2013-04-02 21:34:55 -0600

    Added basic form handling

--- djangotodo/todolist/views.py
+++ djangotodo/todolist/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from todolist.models import Task
+from forms import TaskForm
+from django.core.exceptions import PermissionDenied
 
 def list(request):
-    return render(request, 'todolist/list-view.html', {
-    	'tasks': Task.objects.all()
+	if request.method == "GET":
+		form = TaskForm()
+	elif request.method == "POST":
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+		# make sure to return use a clean form for the resulting page
+		form = TaskForm()
+
+	else:
+		raise PermissionDenied('Only GET and POST methods are allowed')
+
+	return render(request, 'todolist/list-view.html', {
+    	'tasks': Task.objects.all(),
+    	'form': form
     })
